cbabb19774691deedbd5a305f13f1aaf26f57995
Kevin Li <kevin.li-4@student.manchester.ac.uk>
2023-10-28 20:53:39 +0100

    Added uploading and retrieving files to server.js

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const connectDB=require('./config/db')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-
-connectDB()
+// connectDB()
 
 // use imports 
 
@@ -28,13 +29,31 @@ app.get('/homepage', (req, res) => {
 });
 
 
-app.post('/submit', (req, res) => {
-      res.send({ "message": "something"});
-      
-
+app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
+  const files = req.files
+  if (!files) {
+    const error = new Error('Please choose files')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.send(files)
 })
 
+app.get('/photos', (req, res) => {
+  db.collection('mycollection').find().toArray((err, result) => {
+  
+        const imgArray= result.map(element => element._id);
+              console.log(imgArray);
+  
+     if (err) return console.log(err)
+     res.send(imgArray)
+  
+    })
+});
 
+app.post('/submit', (req, res) => {
+  res.send({ "message": "something"});
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
