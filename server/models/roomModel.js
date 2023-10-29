b5bb3c9c691bb43363002983300774aefac46d37
Kevin Li <kevin.li-4@student.manchester.ac.uk>
2023-10-29 05:56:43 +0000

    Set up backend-database process, organised backend

---
+++ server/models/roomModel.js
@@ -0,0 +1,28 @@
+const mongoose= require('mongoose')
+
+const roomSchema = new mongoose.Schema({
+    keyroom: {
+      type: Number,
+      required: true,
+    },
+    hostid: {
+      type: Number,
+      required: true,
+    },
+    password: {
+      type: Number,
+      required: true,
+    },
+    text: {
+      type: String, // This defines an array of strings
+    },
+  });
+  
+  // Create a Mongoose model using the schema
+  const Room = mongoose.model('Room', roomSchema);
+  
+  module.exports = Room;
+  
+  
+  
+  
--- server/server.js
+++ server/server.js
@@ -1,54 +1,35 @@
 const express = require('express');
 const dotenv= require('dotenv').config();
 const app = express();
-const port = 3000;
+const { connectDB } = require('./config/db')
+const port = process.env.PORT || 5000;
 const path = require('path');
-const connectDB=require('./config/db')
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
-
-// connectDB()
-
+const { errorHandler } = require('./middleware/errorMiddleware')
 // use imports 
 
+connectDB()
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
 // app.use(cors({
 //   origin: ['http://localhost:3000'] // only accept requests from localhost:3000 header
 // }));
+app.use('/api/rooms', require('./routes/roomRoutes'))
+
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
-
-
 app.get('/homepage', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/index.html'));
 });
 
-
-app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
-  const files = req.files
-  if (!files) {
-    const error = new Error('Please choose files')
-    error.httpStatusCode = 400
-    return next(error)
-  }
-    res.send(files)
-})
-
-app.get('/photos', (req, res) => {
-  db.collection('mycollection').find().toArray((err, result) => {
-  
-        const imgArray= result.map(element => element._id);
-              console.log(imgArray);
-  
-     if (err) return console.log(err)
-     res.send(imgArray)
-  
-    })
+app.get('/room', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/room.html'));
 });
 
 app.post('/submit', (req, res) => {
